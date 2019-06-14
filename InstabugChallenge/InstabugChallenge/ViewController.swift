00adfd61fc3033541ef0abd9f829aa81979dac80
Omar Samir <omarsamsoliman@gmail.com>
2019-06-14 20:44:36 +0200

    init commit

---
+++ InstabugChallenge/InstabugChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstabugChallenge
+//
+//  Created by Admin on 6/14/19.
+//  Copyright © 2019 Instabug. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
