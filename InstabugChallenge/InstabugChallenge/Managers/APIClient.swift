60554c2dc4a63fb5f5d318b94e637108f094f426
Omar Samir <omarsamsoliman@gmail.com>
2019-06-16 10:47:43 +0200

    fix: handling error in APIClient

--- InstabugChallenge/InstabugChallenge/Managers/APIClient.swift
+++ InstabugChallenge/InstabugChallenge/Managers/APIClient.swift
@@ -20,11 +20,13 @@ class APIClient: NSObject {
     request.httpMethod = "GET"
     let requestTask = URLSession.shared.dataTask(with: request) {
       (data: Data?, response: URLResponse?, error: Error?) in
-      do {
-        let decoder = JSONDecoder()
-        let movies = try decoder.decode(Movies.self, from: data!)
-        completion(movies,nil)
-      } catch _ {
+      if error == nil && data != nil {
+        do {
+          let decoder = JSONDecoder()
+          let movies = try decoder.decode(Movies.self, from: data!)
+          completion(movies,nil)
+        } catch _ {}
+      }else{
         print("Error: \(String(describing: error))")
         completion(nil,error)
       }
